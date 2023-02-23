cb22d95e36069b54c1b9786992e6d06744b4252d
Igor Diniz <igor.ramon14@gmail.com>
2023-02-23 12:52:39 -0300

    Initial Commit

---
+++ video_player/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  video_player
+//
+//  Created by Igor Ramon Fernandes Diniz on 23/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
