e5a211533592e6a4055e1c5972ae3876942984c9
Igor Diniz <igor.ramon14@gmail.com>
2023-03-09 21:10:26 -0300

    feat: player video

--- video_player/ViewController.swift
+++ video_player/ViewController.swift
@@ -6,12 +6,39 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        playVideo()
+    }
+    
+    
+    private func playVideo(){
+        let pathVideo = "adMac"
+        guard let path = Bundle.main.path(forResource: pathVideo, ofType: "mp4") else{
+            debugPrint("\(pathVideo) not found")
+            
+            return;
+        }
+        
+        let player = AVPlayer(url: URL(fileURLWithPath: path))
+        
+        let playerController = AVPlayerViewController()
+        
+        playerController.player = player
+        
+        present(playerController, animated: true){
+            player.play()
+        }
     }
 
 
